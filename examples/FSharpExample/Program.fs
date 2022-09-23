5c0d77b7463e864d12914be6da541f5faeac7d01
antyadev <antyadev@gmail.com>
2022-09-23 10:37:15 +0300

    updated examples

--- examples/FSharpExample/Program.fs
+++ examples/FSharpExample/Program.fs
@@ -1,11 +1,13 @@
 ﻿open System
 open System.IO
 
+open System.Threading.Tasks
 open Microsoft.Extensions.Configuration
 
 open Serilog
 open Serilog.Events
 open Serilog.Sinks.SpectreConsole
+open Spectre.Console
 
 type User = {
     Name: string
@@ -32,6 +34,20 @@ let main argv =
     //    LoggerConfiguration()
     //        .ReadFrom.Configuration(configuration)
     //        .CreateLogger()
+
+    // let status = AnsiConsole.Status()
+    // status.Start("my status", fun ctx ->
+    //     let user = { Name = "test_user" }
+    //     Log.Information($"%A{user}")
+    //     Log.Information("symbol {")
+    //
+    //     Log.Verbose("Verbose level example with {0}", "parameter")
+    //     Log.Debug("Debug level example with {0}", "parameter")
+    //     Log.Information("Information level example with {0}", "parameter")
+    //     Log.Warning("Warning level example with {0}", "parameter")
+    //
+    //     Task.Delay(5000).Wait()
+    // )
 
     let user = { Name = "test_user" }
     Log.Information($"%A{user}")
